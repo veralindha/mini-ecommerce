4a7e4306acc19fb4c1b687993795e2875a8d3384
veralindha <fangkyistiawan@gmail.com>
2023-01-12 07:07:53 +0700

    change quotes element

--- components/utils/quotes.js
+++ components/utils/quotes.js
@@ -4,7 +4,7 @@ export default function Quotes() {
       <div className="container px-5">
         <div className="row gx-5 justify-content-center">
           <div className="col-xl-8">
-            <div className="h2 fs-1 text-white mb-4">"An intuitive solution to a common problem that we all face, wrapped up in a single app!"</div>
+            <div className="h2 fs-1 text-white mb-4">"Halah bacot gawene aa... ojo maning maning ws"</div>
           </div>
         </div>
       </div>
